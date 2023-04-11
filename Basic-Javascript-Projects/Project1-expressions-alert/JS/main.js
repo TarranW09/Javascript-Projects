225ce6e40c4e2347bcbff2ba304d63e7b29b63ca
Tarran Wylie <124862204+TarranW09@users.noreply.github.com>
2023-04-11 15:01:10 -0600

    Add files via upload

---
+++ Basic-Javascript-Projects/Project1-expressions-alert/JS/main.js
@@ -0,0 +1,20 @@
+var A="This is a string";
+window.alert(A);
+document.write("I told the doctor, \"A apple a day keeps you away!\" \"Damn, I was gonna get you!\" Doctor said Damn I will get you. ");
+document.write("\"Do or do not, there is no try."
++ " because Jedi Master Yoda hath said so\""
++ " -Tarran Wylie");
+
+var B = "This is the start of the string" + " This is the end of the string"   //Concatenating strings together//
+document.write(B)
+
+var Family = "The Wylies", Dad = "Reginald", Mom = "Leisa",
+Son1 = "Keltan", Son2 = "Tarran", Son3 = "Caelem";
+document.write(Dad);
+
+document.write(6+4);
+                                                    //Practice Function//
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
