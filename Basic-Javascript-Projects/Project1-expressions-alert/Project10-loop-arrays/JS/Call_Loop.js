ba0658533aa0f6ce4d95e436dda1d91e7a598ed9
Tarran Wylie <124862204+TarranW09@users.noreply.github.com>
2023-04-18 15:34:20 -0600

    Add files via upload

--- Basic-Javascript-Projects/Project1-expressions-alert/Project10-loop-arrays/JS/Call_Loop.js
+++ Basic-Javascript-Projects/Project1-expressions-alert/Project10-loop-arrays/JS/Call_Loop.js
@@ -13,7 +13,48 @@ function Call_Loop() {
     var Y;
     function For_Loop() {
         for (Y = 0; Y < instruments.length; Y++) {
-            Content += Instruments[Y] + "<br>";
+            Content += instruments[Y] + "<br>";
         }
         document.getElementById("List_of_Instruments").innerHTML = Content;
     }
+
+    function Scope() {
+        var carName = "Corvette";
+        document.getElementById("Example").innerHTML = carName;
+    }
+
+    function constant_function() {
+        const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+        Musical_Instrument.color = "blue";
+        Musical_Instrument.price = "$900";
+        document.getElementById("Constant").innerHTML = "The cost of the " +
+            Musical_Instrument.type + " was " + Musical_Instrument.price;
+    }
+
+    var x = 82;
+    document.write(X);
+    {
+        let X = 33;
+        document.write("<br>" + X);
+    }
+    document.write("<br>" + X);
+
+    let car = {
+        make: "Dodge ",
+        model: "Viper ",
+        year: "2021 ",
+        color: "red ",
+        description: function() {
+            return "The car is a " + this.year + this.color + this.make + this.model;
+        }
+    };
+    document.getElementById("Car_Object").innerHTML = car.description();
+
+    function Cont() {
+let text = "";
+for (let i = 0; i < 10; i++) {
+  if (i === 3) { continue; }
+  text += "The number is " + i + "<br>";
+}
+document.getElementById("Cont").innerHTML = text;
+}
